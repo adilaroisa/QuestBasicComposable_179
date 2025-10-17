23c499eeb7bc9b842c27b2430823140db64c5207
adilaroisa <adilaroisa688@gmail.com>
2025-10-17 16:58:47 +0700

    Menambah Image (halloween) dan Text overlay 'My Music'

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -136,7 +136,21 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
         ) {
-
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center
+                )
+            )
         }
     }
 }
